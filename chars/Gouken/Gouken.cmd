859c2b252dc65816595ce3a279bd562a62e70a6b
mredkcm <mredm@email.com>
2026-04-12 23:22:56 -0400

    Gouken can now self cancel

--- chars/Gouken/Gouken.cmd
+++ chars/Gouken/Gouken.cmd
@@ -999,6 +999,7 @@ value=200
 triggerall=!AILevel&&Roundstate=2&&statetype != A&&command != "holddown"&& command="x"
 trigger1=ctrl||stateno=[100,103]
 trigger2=var(4)
+trigger3=(stateno=200||stateno=400||stateno=430)&&time>4
 ;---------------------------------------------------------------------------
 [State -1, Standing Medium Punch]
 type=ChangeState
@@ -1046,6 +1047,7 @@ value=400
 triggerall=!AILevel&&Roundstate=2&&statetype != A&&command="holddown"&& command="x"
 trigger1=ctrl||stateno=[100,103]
 trigger2=var(4)
+trigger3=(stateno=200||stateno=400||stateno=430)&&time>4
 ;---------------------------------------------------------------------------
 [State -1, Crouching Medium Punch]
 type=ChangeState
@@ -1070,6 +1072,7 @@ triggerall=!AILevel&&Roundstate=2&&statetype != A&&command="holddown"&& command=
 trigger1=ctrl||stateno=[100,103]
 trigger2=var(4)
 trigger3=((stateno=[200,209])||(stateno=[400,409]))&&movecontact&&var(55)
+trigger4=(stateno=200||stateno=400||stateno=430)&&time>4
 ;---------------------------------------------------------------------------
 [State -1, Crouching Medium Kick]
 type=ChangeState
